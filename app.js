39dae12ada66d8e97daf328c04d40b75bacd4b71
newjuna <zxcv6309@gmail.com>
2026-06-16 14:45:03 +0900

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@
  * ============================================================ */
 
 // ★★★ 여기에 Apps Script 배포 URL을 붙여넣으세요 ★★★
-const API_URL = 'https://script.google.com/macros/s/AKfycbwYyY7iT3k_X7jJ7q3q3_X7jJ7q3_X7jJ7q3_X7j/exec'; 
+const API_URL = 'https://script.google.com/macros/s/AKfycbyE-e_Z-gWTqktkCf-4ZfZfY6a_CGsWDxyj4Alyf9raVjbd1EJOFd_2BB6wKdEPquK-SQ/exec'; 
 
 /* ============ CI 컬러 ============ */
 const CI_RED  = '#E60033';
